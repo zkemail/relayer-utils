f3a74bf0193583ed5cf54d64f96740ea3f907595
Dimitri <dimitridumonet@gmail.com>
2025-02-11 17:18:29 +0700

    chore: expose genFromDecomposed

--- ts_tests/parse_email.ts
+++ ts_tests/parse_email.ts
@@ -2,11 +2,12 @@
  * TODO: Make jest ignore this
  * This file does not work with jest, so for now we renamed it so it doesn't run.
  */
- // TODO: Can only run one test file at a time, since init() will colide
+// TODO: Can only run one test file at a time, since init() will colide
 import { expect, test, describe } from "bun:test";
 
 import { parseEmail, init } from "../pkg";
 import airbnbEmail from "./airbnb_eml";
+import airbnbEmailInvalid from "./airbnb_eml_invalid";
 
 describe("Parse email test suite", async () => {
   await init();
@@ -25,5 +26,41 @@ describe("Parse email test suite", async () => {
       return;
     }
     throw new Error("Parsed invalid email");
+  });
+
+  test("Should provide descriptive error for empty email", async () => {
+    try {
+      await parseEmail("");
+      throw new Error("Should have failed");
+    } catch (err) {
+      expect(err.toString()).toContain("Invalid email: Email cannot be empty");
+    }
+  });
+
+  test("Should provide descriptive error for invalid email format", async () => {
+    try {
+      await parseEmail("invalid-email-format");
+      throw new Error("Should have failed");
+    } catch (err) {
+      expect(err.toString()).toContain("Invalid email: Email must contain @ symbol");
+    }
+  });
+
+  test("Should provide descriptive error for malformed DKIM signature", async () => {
+    try {
+      await parseEmail(airbnbEmailInvalid);
+      throw new Error("Should have failed");
+    } catch (err) {
+      expect(err.toString()).toContain("Failed to parse email: Invalid DKIM signature");
+    }
+  });
+
+  test("Should not be able to use regex lookaheads", async () => {
+    try {
+      await parseEmail(airbnbEmailInvalid);
+      throw new Error("Should have failed");
+    } catch (err) {
+      expect(err.toString()).toContain("Failed to parse email: Invalid DKIM signature");
+    }
   });
 });
